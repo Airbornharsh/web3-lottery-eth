21587f3bdf0c2fda31e642dfab04a84812258363
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-07-04 10:53:44 +0530

    Added Functions in Lottery Context

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,37 @@
 'use client'
+import { useLottery } from '@/context/LotteryContext'
 import { useWallet } from '@/context/WalletContext'
-import { getEthersSigner } from '@/lib/web3'
+import { Button } from '@chakra-ui/react'
 import React from 'react'
 
 const Page = () => {
   const { isConnected } = useWallet()
+  const {
+    getParticipants,
+    startLottery,
+    enterLottery,
+    getWinners,
+    pickWinners,
+  } = useLottery()
   return (
     <div>
-      {isConnected ? <div onClick={async () => {}}>test</div> : 'Not connected'}
+      {isConnected ? (
+        <div className="flex flex-col gap-2">
+          <Button
+            onClick={() => {
+              startLottery({ duration: 1 * 60 * 1000 })
+            }}
+          >
+            Start Lottery
+          </Button>
+          <Button onClick={enterLottery}>Enter Lottery</Button>
+          <Button onClick={getParticipants}>Get participants</Button>
+          <Button onClick={pickWinners}>Pick Winners</Button>
+          <Button onClick={getWinners}>Get Winners</Button>
+        </div>
+      ) : (
+        'Not connected'
+      )}
     </div>
   )
 }
--- src/context/LotteryContext.tsx
+++ src/context/LotteryContext.tsx
@@ -1,7 +1,16 @@
 'use client'
+import { ABI, CONTRACT_ADDRESS } from '@/lib/config'
+import { getEthersSigner } from '@/lib/web3'
+import { ethers } from 'ethers'
 import React, { createContext, useContext, useState, ReactNode } from 'react'
 
-interface LotteryContextProps {}
+interface LotteryContextProps {
+  getParticipants: () => Promise<void>
+  startLottery: ({ duration }: { duration: number }) => Promise<void>
+  enterLottery: () => Promise<void>
+  pickWinners: () => Promise<void>
+  getWinners: () => Promise<void>
+}
 
 const LotteryContext = createContext<LotteryContextProps | undefined>(undefined)
 
@@ -22,7 +31,82 @@ interface LotteryContextProviderProps {
 export const LotteryProvider: React.FC<LotteryContextProviderProps> = ({
   children,
 }) => {
-  const contextValue: LotteryContextProps = {}
+  const [participants, setParticipants] = useState<any[]>([])
+
+  const startLottery = async ({ duration }: { duration: number }) => {
+    const signer = await getEthersSigner()
+    if (!signer) return
+
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer)
+    try {
+      const result = await contract.startLottery(duration)
+      console.log('result:', result)
+    } catch (error) {
+      console.error('Error starting lottery:', error)
+    }
+  }
+
+  const enterLottery = async () => {
+    const signer = await getEthersSigner()
+    if (!signer) return
+
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer)
+    try {
+      const result = await contract.enterLottery('Harsh', {
+        value: 15000000000000,
+      })
+      console.log('result:', result)
+    } catch (error) {
+      console.error('Error entering lottery:', error)
+    }
+  }
+
+  const getParticipants = async () => {
+    const signer = await getEthersSigner()
+    if (!signer) return
+
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer)
+    try {
+      const result = await contract.getParticipants()
+      console.log('result:', result)
+    } catch (error) {
+      console.error('Error reading data:', error)
+    }
+  }
+
+  const pickWinners = async () => {
+    const signer = await getEthersSigner()
+    if (!signer) return
+
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer)
+    try {
+      const result = await contract.pickWinners()
+      console.log('result:', result)
+    } catch (error) {
+      console.error('Error picking winners:', error)
+    }
+  }
+
+  const getWinners = async () => {
+    const signer = await getEthersSigner()
+    if (!signer) return
+
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer)
+    try {
+      const result = await contract.getWinners()
+      console.log('result:', result)
+    } catch (error) {
+      console.error('Error reading data:', error)
+    }
+  }
+
+  const contextValue: LotteryContextProps = {
+    getParticipants,
+    startLottery,
+    pickWinners,
+    getWinners,
+    enterLottery,
+  }
 
   return (
     <LotteryContext.Provider value={contextValue}>
